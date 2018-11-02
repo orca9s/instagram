f326e2e812da3826ac2a47c6eb5d7a2d7c3d1493
jsm <sang93423@gmail.com>
2018-11-02 11:42:13 +0900

    posts, members app추가 및 기본 모델들 migration생성

--- app/posts/models.py
+++ app/posts/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class Post(models.Model):
     author = models.ForeignKey(
-        'User',
+        # <AppName>.<ModelName>
+        'members.User',
         on_delete=models.CASCADE,
     )
     photo = models.ImageField(upload_to='post')
@@ -15,7 +16,18 @@ class Comment(models.Model):
         on_delete=models.CASCADE,
     )
     author = models.ForeignKey(
-        'User',
+        'members.User',
         on_delete=models.CASCADE,
     )
     content = models.TextField()
+    tags = models.ManyToManyField(
+        'HashTag',
+        blank=True,
+    )
+
+
+class HashTag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
