b22b621a11999b24f9fcd9ecb29d581eac7e9e96
jsm <sang93423@gmail.com>
2018-11-19 12:45:05 +0900

    bootstrap다운받고 적용

--- app/config/settings.py
+++ app/config/settings.py
@@ -18,6 +18,7 @@ ROOT_DIR = os.path.dirname(BASE_DIR)
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # 사용자가 업로드한 파일이 저장될 Base디렉토리(settings.MEDIA_URL)
 MEDIA_ROOT = os.path.join(ROOT_DIR, '.media')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 # 유저가 업로드한 파일에 접근하고자 할 때 prfix URL (settings.MEDIA_URL)
 # FileField, MediaField의 URL이 아래 설정 기준으로 바
 MEDIA_URL = '/media/'
@@ -35,6 +36,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
 
 # Application definition
 
