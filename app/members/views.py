065310e7fb4f414495f09855fcafdcbc8ed887f2
jsm <sang93423@gmail.com>
2018-11-23 12:49:39 +0900

    login처리 및 base.html에서 로그인 여부버튼 표시

--- app/members/views.py
+++ app/members/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
 from members.forms import LoginForm
 
@@ -16,10 +17,29 @@ def login_view(request):
     #   username, password를 받을 수 있도록 함
     #       password는 widget에 PasswordInput을 사용하기
     if request.method == 'POST':
-        pass
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('posts:post-list')
+        else:
+            # 인증 실패시
+            pass
+        # 1. request.POST에 데이터가 옴
+        # 2. 온 데이터 중에서 username에 해당하는 값과 password에 해당하는 값을 각각
+        #    username, password변수에 할
+        # 3. 사용자 인증을 수행
+        #    username/password에 해당하는 사용자가 있는지 확인
+        # 4-1. 인증에 성공한다면
+        #      세션/쿠키 기반의 로그인과정을 수행, 완료 후 posts:post-list 페이지로 redirect
+        # 4-2. 인증에 실패한다면
+        #      이 페이지에서 인증에 실패했음을 사용자에게 알려줌
     else:
         form = LoginForm()
         context = {
             'form': form,
         }
         return render(request, 'members/login.html', context)
+
