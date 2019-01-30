96a841bdea43a83b0ca99ab87e20f0cc30624e8b
jsm <sang93423@gmail.com>
2019-01-30 16:41:09 +0900

    members/models.py에 like_post_toggle기능추가 포스트 좋아요 또는 좋아요 취소

--- app/members/models.py
+++ app/members/models.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.staticfiles.templatetags.staticfiles import static
 from django.db import models
 
+from posts.models import PostLike
+
 
 class User(AbstractUser):
     img_profile = models.ImageField(
@@ -31,3 +33,20 @@ class User(AbstractUser):
             return self.img_profile.url
         # 없다면 static폴더에서 뒷 경로 파일의 url을 리턴
         return static('images/blank_user.png')
+
+    def like_post_toggle(self, post):
+        # 전달받은 post에 대한 Like를 Toggle처리
+        # PostLike.objects.filter(
+        #     post=post,
+        #     user=self,
+        # ).exists()
+        # if self.postlike_set.filter(post=post).exists():
+        #     self.postlike_set.filter(post=post).delete()
+        # else:
+        #     self.postlike_set.create(post=post)
+        # 자신에게 연결된 PostLike중, post값이 매개변수의 post인 PostLike가 있다면 가져오고, 없으면 생성
+        postlike, postlike_create = self.postlike_set.get_or_create(post=post)
+        # 생성되었다면 없다가 생겼다는 말이므로(새로 좋아요를 누름) 따로 처리 필요 없음
+        # 생성되지 않았다면 이미 있었다는 말이므로 toggle처리를 위해 삭제
+        if not postlike_create:
+            postlike.delete()
